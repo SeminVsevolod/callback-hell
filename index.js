78a6790795ffa150356ac73cbb10d97dc63fc41d
SeminVsevolod <semin.vsevolod@gmail.com>
2020-06-14 19:27:49 +0300

    waiting for multiple promises simultaneously

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
-const superagent = require("superagent");
+const fs = require('fs');
+const superagent = require('superagent');
 
 const readFilePro = (file) => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
-      if (err) reject("I could not find that file 😭");
+      if (err) reject('I could not find that file 😭');
       resolve(data);
     });
   });
@@ -24,28 +24,31 @@ const getDogPic = async () => {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    const result = await superagent.get(
-      `https://dog.ceo/api/breed/${data}/images/random`
-    );
-    console.log("result.body.message", result.body.message);
+    const result1Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    const result2Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    const result3Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
 
-    await writeFilePro("dog.img.txt", result.body.message);
-    console.log("Random dog will saved to file!");
+    const all = await Promise.all([result1Pro, result2Pro, result3Pro]);
+    const images = all.map((el) => el.body.message);
+    console.log('images->', images);
+
+    await writeFilePro('dog.img.txt', images.join('\n'));
+    console.log('Random dog images will saved to file!');
   } catch (err) {
-    console.log("err -> ", err);
+    console.log('err -> ', err);
     throw err;
   }
-  return "2: READY 🐕";
+  return '2: READY 🐕';
 };
 
 (async () => {
   try {
-    console.log("1: Will get dog pics!");
+    console.log('1: Will get dog pics!');
     const x = await getDogPic();
     console.log(x);
-    console.log("3: Done getting dog pics!");
+    console.log('3: Done getting dog pics!');
   } catch (err) {
-    console.log("ERROR 💣");
+    console.log('ERROR 💣');
   }
 })();
 
